f3d37e67d651754252f3aaa151c064a61af6e4f0
Swarup <swarupkum45@gmail.com>
2022-12-24 08:37:38 +0530

    update done

---
+++ frontend/src/App.js
@@ -0,0 +1,82 @@
+// eslint-disable-next-line
+
+import React from "react";
+import Tasks from "./Tasks";
+import { Paper, TextField } from "@material-ui/core";
+import { Checkbox, Button } from "@material-ui/core";
+import "./App.css";
+const date = require(__dirname + "/date.js");
+
+const today = new Date();
+
+class App extends Tasks {
+    state = { tasks: [], currentTask: "" };
+    // const today = new Date();
+
+    render() {
+        const { tasks } = this.state;
+        return (
+            <div className="App flex">
+                
+                <Paper elevation={3} className="container">
+                    <div className="heading">Welcome</div>
+                    <form
+                        onSubmit={this.handleSubmit}
+                        className="flex"
+                        style={{ margin: "15px 0" }}
+                    >
+                        <TextField
+                            variant="outlined"
+                            size="small"
+                            style={{ width: "80%" }}
+                            value={this.state.currentTask}
+                            required={true}
+                            onChange={this.handleChange}
+                            placeholder="Add New TO-DO Item"
+                        />
+                        <Button
+                            style={{ height: "40px" }}
+                            color="primary"
+                            variant="outlined"
+                            type="submit"
+                        >
+                            Add task
+                        </Button>
+                    </form>
+                    <div>
+                        {tasks.map((task) => (
+                            <Paper
+                                key={task._id}
+                                className="flex task_container"
+                            >
+                                <Checkbox
+                                    checked={task.completed}
+                                    onClick={() => this.handleUpdate(task._id)}
+                                    color="primary"
+                                />
+                                <div
+                                    className={
+                                        task.completed
+                                            ? "task line_through"
+                                            : "task"
+                                    }
+                                >
+                                    {task.task}
+                                </div>
+                                <Button
+                                    onClick={() => this.handleDelete(task._id)}
+                                    color="secondary"
+                                >
+                                    delete
+                                </Button>
+                            </Paper>
+                        ))}
+                    </div>
+                </Paper>
+
+            </div>
+        );
+    }
+}
+
+export default App;
